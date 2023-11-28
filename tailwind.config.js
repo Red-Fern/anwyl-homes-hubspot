7e442420bba8ed295a9401c53da6e30327993c9f
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2023-11-28 16:56:48 +0000

    Fix tailwind content configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: './anwyl-homes/**/*.html',
+  content: [
+    './anwyl-homes/**/*.html'
+  ],
   theme: {
     extend: {}
   corePlugins: {
